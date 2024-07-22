4d1a42ea0bd18ec035e522c70f2c478b5a9f1cb2
UnaizaZafar <unaizaofficial840@gmail.com>
2024-07-22 21:15:39 +0500

    HomePage Completed

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Hero from '../component/Hero'
-import HomeCards from '../component/HomeCards'
+
 function StartPage() {
   return (
     <div>
-      <Hero title="Become a React Dev" subtitle="Find the React job that fits your skills and needs"/>
-      <HomeCards/>
+  
     </div>
   )
 }
