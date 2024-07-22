1f282a9edcc98da4b79e932a184eef471a8c600e
UnaizaZafar <unaizaofficial840@gmail.com>
2024-07-22 21:15:39 +0500

    HomePage Completed

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import logo from "D:/AgIT/react-jobs/src/images/React_(web_framework)-Logo.wine.png";
 
 function Navbar() {
@@ -8,31 +8,61 @@ function Navbar() {
       <nav className="bg-indigo-800 border-b border-indigo-600 text-white">
         <ul className="grid  grid-rows-1 grid-cols-2 mx-20 ">
           <div className="flex items-center ">
-            <Link to="/">
+            <NavLink to="/">
               <li className="p-5">
                 <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
                   <image href={logo} width="50" height="50" alt="" />
                 </svg>
               </li>
-            </Link>
-            <Link to="/">
+            </NavLink>
+            <NavLink to="/">
               <li className="p-5 text-2xl font-bold">
                 <h1>React Jobs</h1>
               </li>
-            </Link>
+            </NavLink>
           </div>
           <div className="flex flex-row-reverse items-center font-medium">
-          <Link to="/add-job">
-              <li className="mx-3 active:bg-black active:p-2  rounded-md">Add Job</li>
-            </Link>
-            <Link to="/jobs">
-              <li className="mx-3 active:bg-black active:p-2 rounded-md">Jobs</li>
-            </Link>
-            <Link to="/">
-              <li className="mx-3 active:bg-black active:p-2 rounded-md">Home</li>
-            </Link>
-           
-            
+            <NavLink
+              to="/add-job"
+              style={({ isActive }) => {
+                return {
+                  background: isActive ? "black" : "",
+                  padding: isActive ? "6px" : "",
+                  borderRadius: isActive ? "8px" : "",
+                };
+              }}
+            >
+               <li className="mx-3 active:bg-black active:p-2 rounded-md">
+              Add Job</li>
+            </NavLink>
+            <NavLink
+              to="/jobs"
+              style={({ isActive }) => {
+                return {
+                  background: isActive ? "black" : "",
+                  padding: isActive ? "6px" : "",
+                  borderRadius: isActive ? "8px" : "",
+                };
+              }}
+            >
+              <li className="mx-3 active:bg-black active:p-2 rounded-md">
+                Jobs
+              </li>
+            </NavLink>
+            <NavLink
+              to="/"
+              style={({ isActive }) => {
+                return {
+                  background: isActive ? "black" : "",
+                  padding: isActive ? "6px" : "",
+                  borderRadius: isActive ? "8px" : "",
+                };
+              }}
+            >
+              <li className="mx-3 active:bg-black active:p-2 rounded-md">
+                Home
+              </li>
+            </NavLink>
           </div>
         </ul>
       </nav>
