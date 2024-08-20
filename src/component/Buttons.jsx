c2ad919c7bd4ba42276c739fb7ee95027c1d2fd8
UnaizaZafar <unaizaofficial840@gmail.com>
2024-08-20 21:29:16 +0500

    Update Buttons.jsx

--- src/component/Buttons.jsx
+++ src/component/Buttons.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Buttons({bg='bg-black'}) {
+function Buttons({bg='bg-black',children}) {
   return (
     <>
-    <div className={`${bg} p-3 text-white rounded-lg mt-2 mb-4`}>
+    <div className={`${bg} p-3 text-white rounded-lg mt-2 mb-4`}>{children}
       </div>
     </>
   )
